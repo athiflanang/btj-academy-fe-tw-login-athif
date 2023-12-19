450f91393773d2c04828777dcef0f4995a270b87
Athif <120725167+athiflanang@users.noreply.github.com>
2023-12-19 19:38:06 +0700

    Add files via upload

---
+++ TAILWIND/tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    DEFAULT: "#F28705",
+                    600: "#DC7B05",
+                    50: "#FEF3E6",
+                },
+                black: {
+                    DEFAULT: "#2A2A2A",
+                    10: "#1A1A1A",
+                },
+                'gray' : {
+                    custom: "#1C1C1C",
+                    10: "#C0C0C0",
+                }    
+            },
+            fontFamily: {
+                sans: ["IBM Plex Sans", "sans-serif"],
+            }
+        },
+    },
+    plugins: [require("@tailwindcss/forms")],
+}
